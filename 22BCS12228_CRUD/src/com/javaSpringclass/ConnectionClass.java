1d6f3e4880c26e86bb085eb76e43e1ea2380cf04
jeete <jeete@SENTINEL>
2024-02-07 15:39:36 +0530

    done

---
+++ 22BCS12228_CRUD/src/com/javaSpringclass/ConnectionClass.java
@@ -0,0 +1,18 @@
+package com.javaSpringclass;
+
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionClass {
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		Connection con=null;
+		Class.forName("com.mysql.cj.jdbc.Driver");//Load and register driver
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/cuims","root","1234");
+		System.out.println("Connection Established");
+		return con;
+	}
+}
